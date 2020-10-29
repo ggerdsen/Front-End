4c0cfe91d528d43ee35d7c8ec719fb41c166b932
Judith Pillado <judith.pillado@gmail.com>
2020-10-29 12:46:23 -0500

    Add initial route and devise route
    Co-authored-by: Priya Power <49959312+priyapower@users.noreply.github.com>
    Co-authored-by: Cam C <57038617+ckccameron@users.noreply.github.com>
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Corey Carter <59756152+coreycartercodes@users.noreply.github.com>
    Co-authored-by: Garrett Gerdsen <45319985+ggerdsen@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
+  devise_for :teachers, controllers: { omniauth_callbacks: 'teachers/omniauth_callbacks' }
+  devise_scope :teacher do
+    get 'teachers/sign_in', to: 'teachers/sessions#new', as: :new_teacher_session
+    get 'teachers/sign_out', to: 'teachers/sessions#destroy', as: :destroy_teacher_session
+  end
 end
