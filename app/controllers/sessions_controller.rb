1ed402b3913db488b3c1dad8f0e0cf7ab89e8f6f
Cam C <ckc.cameron@gmail.com>
2020-10-29 19:15:12 -0700

    Begin building create method for SessionsController
    * left notes to summarize what we did and guide what we think comes next
    
    Co-authored-by: Cam C <57038617+ckccameron@users.noreply.github.com>
    Co-authored-by: Priya Power <49959312+priyapower@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def create
+    * looks like access token is taken care of by omniauth gem
+    * you can confirm this in pry session by calling request.env['omniauth.auth']
+    and this will show the hash with the info we need for OAuth2
+    * trying to post to backend api database so that the Student user is actually saved
+    * we think the Student model method in the backend repo (update_or_create) will be called in Api Students controller
+    client_id = ENV['GOOGLE_CLIENT_ID']
+    client_secret = ENV['GOOGLE_CLIENT_SECRET']
+    conn = Faraday.new(url: 'http://localhost:3000/api/v1', headers: {'Accept': 'application/json'})
+    response = conn.post('/students') do |req|
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
+      req.params['omniauth'] = request.env['omniauth.auth']
+    end
+    binding.pry
+    # "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{params["id_token"]}"
   end
 end
