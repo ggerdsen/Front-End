ecfbddeed749879d0db89851de28334b778870ae
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-10-31 20:14:53 -0500

    SUBJECT: Updates the Create Action in Sessions Controller
    Co-authored-by: Cam C <57038617+ckccameron@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,26 +11,21 @@ class SessionsController < ApplicationController
       access_token: request.env['omniauth.auth']
     }
     id = request.env['omniauth.auth'][:uid]
-    # new api request: am i in the system? yes, redirect_to dashboard. no, redirect to registration
-    response = Faraday.get("http://localhost:3000/api/v1/#{session[:user_type]}/#{id}") do |request|
-      request.params[:id] = id
+    session[:uid] = id
+
+    # response = Faraday.get("http://localhost:3000/api/v1/#{session[:user_type]}/#{id}") do |request|
+    #   request.params[:id] = id
+    # end
+
+    response2 = Faraday.post("http://localhost:3000/api/v1/#{session[:user_type]}") do |request|
+      request.body = body
     end
 
-    # response2 = Faraday.post("http://localhost:3000/api/v1/#{session[:user_type]}") do |request|
-    #   request.body = body
-    # end
 
     if session[:user_type] == 'students'
-      redirect_to '/student/registration'
+      redirect_to '/student/dashboard'
     else
       redirect_to '/teacher/registration'
-      # conn = Faraday.new(url: 'http://localhost:3000/api/v1', headers: {'Accept': 'application/json'})
     end
-    # response = conn.post('/students') do |req|
-    #   req.params['client_id'] = client_id
-    #   req.params['client_secret'] = client_secret
-    #   req.params['omniauth'] = request.env['omniauth.auth']
-    # end
-    # "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{params["id_token"]}"
   end
 end
