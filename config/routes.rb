c44e66492f129bd98f5d20f8d12ebe1a07e21f84
Philip DeFraties <philipjfry338@gmail.coom>
2020-11-01 15:17:45 -0700

    Adds courses#roster path, as well as teacher#show (for testing link functionality)

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   get '/auth/google_oauth2/callback', to: 'sessions#create'
-
+  resources :teachers, only: [:show]
   namespace :teachers do
     # resources :courses, only: [:show]
     get '/courses/:course_id', to: 'courses#show'
+    get '/courses/:course_id/roster', to: 'courses#roster'
     get '/courses/:course_id/edit', to: 'courses#edit'
     patch '/courses/:course_id', to: 'courses#update'
   end
