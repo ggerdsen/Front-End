d3efb946a7e869bf7e9b28170eb253487e54fa91
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-10-31 20:02:19 -0500

    SUBJECT: Adds new expectations based on radio buttons being added to this viewing page
    Co-authored-by: Cam C <57038617+ckccameron@users.noreply.github.com>

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe 'User Logs In' do
   scenario "using google oauth2" do
     stub_omniauth
     visit root_path
-    expect(page).to have_link("Sign in with Google")
-    click_link "Sign in with Google"
-    expect(page).to have_content("John Smith")
+    choose(option: 'students')
+    # expect(page).to have_link("Sign in with Google")
+    click_on "Sign in with Google"
+    expect(page).to have_content("John")
+    expect(page).to have_content("Smith")
     expect(page).to have_link("Logout")
   end
 end
