ca8d7e6b82e988d7bcd3012adaf3586a6f770492
Corey Carter <crc2231@gmail.com>
2020-11-01 12:43:47 -0700

    Add helper methods for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
-
+  helper_method :cart,
+                :current_user,
+                :current_teacher?,
+                :current_student?
   def current_user
-    # if session[:user_type] = 'students'
-      @current_user ||= Student.find(session[:uid]) if session[:uid]
-    # else
-    #   @current_user ||= Teacher.find(session[:uid]) if session[:uid]
-    # end
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def current_student?
+    current_user && current_user.student_id
+  end
+
+  def current_teacher?
+    current_user && current_user.teacher_id
+  end
+
+  def require_user
+    render file: 'public/404', status: 404 unless current_user
+  end
+
+  def require_teacher
+    render file: 'public/404', status: 404 unless current_teacher?
+  end
+
+  def generate_flash(resource)
+    resource.errors.messages.each do |validation, message|
+      flash[validation] = "#{validation}: #{message}"
+    end
+  end
+
 end
