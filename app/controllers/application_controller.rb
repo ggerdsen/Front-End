05de1f7673050d72e84094598a6bf5ce0893e132
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-10-31 20:17:56 -0500

    SUBJECT: Adds helper method current_user to the Application Controller
    Co-authored-by: Cam C <57038617+ckccameron@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    # if session[:user_type] = 'students'
+      @current_user ||= Student.find(session[:uid]) if session[:uid]
+    # else
+    #   @current_user ||= Teacher.find(session[:uid]) if session[:uid]
+    # end
+  end
 end
