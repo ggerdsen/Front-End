a985814a257a5868d200a0b2ebae44d0811c3841
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-01 17:23:56 -0700

    change edit spec

--- spec/requests/teachers/courses/edit_spec.rb
+++ spec/requests/teachers/courses/edit_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'Teacher edits a course' do
     Faraday.new(url)
   end
 
-  describe 'edit' do
+  describe 'edit and delete' do
     it 'can consume an api and edit a course' do
       response = Faraday.get('http://localhost:3000/api/v1/teachers/courses')
       courses = JSON.parse(response.body, symbolize_names: true)
@@ -37,6 +37,27 @@ RSpec.describe 'Teacher edits a course' do
       expect(course[:attributes][:name]).to eq(name)
       expect(course[:attributes][:course_code]).to eq(course_code)
       expect(course[:attributes][:school_name]).to eq(school_name)
+    end
+
+    it 'can create and delete a course' do
+      #create
+      stub_omniauth
+      name = 'Couseling Theories'
+      course_code = 1234
+      school_name = 'Whitworth'
+
+      body = {
+        name: name,
+        course_code: course_code,
+        school_name: school_name
+      }
+
+      response = conn('/api/v1/teachers/courses').post do |request|
+        request.body = body
+      end
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      require "pry"; binding.pry
     end
   end
 end
