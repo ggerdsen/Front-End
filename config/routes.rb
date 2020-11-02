51e9803b8a77c498b1d6fa65dfd01182f066e38a
Cam C <ckc.cameron@gmail.com>
2020-11-01 16:13:12 -0800

    Add routes for logout and teacher dashboard
    Co-authored-by: Arique1104 <60521756+Arique1104@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   get '/auth/google_oauth2/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   post '/registration', to: 'welcome#create_user'
 
   get '/student/dashboard', to: 'students#dashboard'
 
-  get '/teacher/registration', to: 'teachers#new'
+  get '/teacher/dashboard', to: 'teachers#dashboard'
 end
