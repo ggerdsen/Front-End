d6b9e867b9bbd240fe804908317734ce663dc83b
Cam C <ckc.cameron@gmail.com>
2020-11-02 13:48:45 -0800

    Change route to add teachers into find by uid response from backend
    Co-authored-by: Arique1104 <60521756+Arique1104@users.noreply.github.com>

--- app/facades/teacher_facade.rb
+++ app/facades/teacher_facade.rb
@@ -1,9 +1,9 @@
 class TeacherFacade
   def self.find(uid)
-    response = Faraday.get("http://localhost:3000/api/v1/find/#{uid}")
+    response = Faraday.get("http://localhost:3000/api/v1/teachers/find/#{uid}")
 
     parsed_data = JSON.parse(response.body, symbolize_names: true)
-    
+
     Teacher.new(parsed_data)
   end
 end
