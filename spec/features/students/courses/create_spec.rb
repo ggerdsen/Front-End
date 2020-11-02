2108d009d2c3d826a44f2767006b686d47cdbfad
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-01 17:23:04 -0700

    Update for typos on paths and expectations
    Co-authored-by: Saryn Mooney
    <smooney15@my.whitworth.edu>

--- spec/features/students/courses/create_spec.rb
+++ spec/features/students/courses/create_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Student enrolls in a course' do
   scenario "as a logged in student" do
     stub_omniauth
     # This will need to be updated once we have authorization for student users versus teacher users
-    visit student_courses
+    visit students_courses_path
     # EVENTUALLY, we need to update for dynamic since this test won't pass on anyone elses local
         # faraday call on a teacher, on their course, who are their students?
 
@@ -12,14 +12,14 @@ RSpec.describe 'Student enrolls in a course' do
     # teacher id 4 Rubi Feest
     course_name = 'Arts'
     class_code = 'n5d7j7hb'
-    within 'my-classes' do
+    within '#my-classes' do
       expect(page).to_not have_content(course_name)
     end
 
-    fill_in 'enrollment', with: class_code
+    fill_in :enrollment, with: class_code
     click_on 'Add Course'
-    expect(current_path).to eq(student_courses)
-    within 'my-classes' do
+    expect(current_path).to eq(students_courses_path)
+    within '#my-classes' do
       expect(page).to have_content(course_name)
     end
   end
