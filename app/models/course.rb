e7c5b4d551568de2778445c7ac9185d89477cf90
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-02 09:07:04 -0700

    Add course poro
    Co-authored-by: Arique1104 <60521756+Arique1104@users.noreply.github.com>

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,17 @@
 class Course
-  attr_reader :name,
-              :id
+  attr_reader :id,
+              :name,
+              :course_code,
+              :school_name,
+              :teacher_id,
+              :course_points
 
   def initialize(attrs)
-    @name = attrs[:data][0][:attributes][:name]
-    @id = attrs[:data][0][:id]
+    @id = attrs[:id]
+    @name = attrs[:attributes][:name]
+    @course_code = attrs[:attributes][:course_code]
+    @school_name = attrs[:attributes][:school_name]
+    @teacher_id = attrs[:attributes][:teacher_id]
+    @course_points = attrs[:attributes][:course_points]
   end
 end
