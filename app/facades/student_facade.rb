abf4a51c64fcaa47be343d7de8a8fe04878c8285
Cam C <ckc.cameron@gmail.com>
2020-11-02 13:44:16 -0800

    Change route to add students into find by uid response from backend
    Co-authored-by: Arique1104 <60521756+Arique1104@users.noreply.github.com>

--- app/facades/student_facade.rb
+++ app/facades/student_facade.rb
@@ -1,9 +1,9 @@
 class StudentFacade
-  def self.find(uid)
-    response = Faraday.get("http://localhost:3000/api/v1/find/#{uid}")
+  def self.find(uid)                         
+    response = Faraday.get("http://localhost:3000/api/v1/students/find/#{uid}")
 
     parsed_data = JSON.parse(response.body, symbolize_names: true)
-    
+
     Student.new(parsed_data)
   end
 end
