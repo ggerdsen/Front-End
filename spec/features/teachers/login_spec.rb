b425638107aa8e00558eb9ffd58eafa696c955e3
Cam C <ckc.cameron@gmail.com>
2020-11-02 17:02:22 -0800

    Write teacher login/logout spec
    Co-authored-by: Arique1104 <60521756+Arique1104@users.noreply.github.com>

---
+++ spec/features/teachers/login_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "teacher login" do
+  describe "using google oauth2" do
+    it "allows teacher to login" do
+      VCR.use_cassette("google_oauth2_teacher", allow_playback_repeats: true) do
+        visit root_path
+
+        stub_teacher_omniauth
+
+        choose(option: 'teachers')
+
+        click_on "Sign in with Google"
+
+        expect(page).to have_content("Giovanni")
+        expect(page).to have_button("Log Out")
+        expect(current_path).to eq("/teacher/courses")
+      end
+    end
+
+    # it "allows student to logout" do
+    #   VCR.use_cassette("google_oauth2_teachers", allow_playback_repeats: true) do
+    #     visit root_path
+    #
+    #     stub_teacher_omniauth
+    #
+    #     choose(option: 'teachers')
+    #
+    #     click_on "Sign in with Google"
+    #
+    #     expect(current_path).to eq("/teacher/courses")
+    #
+    #     click_button "Log Out"
+    #
+    #     expect(current_path).to eq(root_path)
+    #   end
+    # end
+  end
+end
