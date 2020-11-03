6db79fb91f000158f8668e93f04d2b7653e74e81
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-11-03 16:55:48 -0600

    Adds teacher controller path with update action.
    DETAILS: This is where the teacher is given the opportunity to update their school name and school district.
    
    Co-authored-by: Cam C <57038617+ckccameron@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   get '/auth/google_oauth2/callback', to: 'sessions#create'
+
   get '/logout', to: 'sessions#destroy'
 
   post '/registration', to: 'welcome#create_user'
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
   namespace :student do
     resources :courses, only: [:show, :index]
    end
+
+   resources :teacher, only: [:update]
 
   namespace :teacher do
     resources :courses, only: [:show, :index]
