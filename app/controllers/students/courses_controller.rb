262bd0ead337230d8b423620e880865e94cf5dd7
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-03 13:24:08 -0700

    Clean up student course controller

--- app/controllers/students/courses_controller.rb
+++ app/controllers/students/courses_controller.rb
@@ -5,10 +5,7 @@ class Students::CoursesController < ApplicationController
   end
 
   def index
-    # @student = current_user[:uid]
-    student_id = 1
-    # From postico (priyas local), student 4 is who we are using "Cinthia"
-    # From postico (Saryn's local), student 4 is who grabbed (Fidel Ryan DDS)
+    student_id = 1 # @student = current_user[:uid]
     response = conn("/api/v1/students/#{student_id}").get
     student = JSON.parse(response.body, symbolize_names: true)
     student_course_params = {student_id: student[:data][:id].to_i}
@@ -20,30 +17,15 @@ class Students::CoursesController < ApplicationController
     @courses = json[:data].map do |course_data|
       Course.new(course_data)
     end
-    # NEED TO CALL OUR COURSES
-    # @courses = sdlkfsldfkjsdlfkj
-
-    # response = Faraday.get("BACKEND URI")
-    # courses_data = JSON.parse(response.body, symbolize_names: true)
-    # @courses = courses_data[:data].map do |course_data|
-    #   Course.new(course_data)
-    # end
   end
 
   def create
-    # what comes through should be the course code
     response = conn("/api/v1/teachers/courses/find?courseCode=#{student_course_params[:enrollment]}").get
     exists = JSON.parse(response.body, symbolize_names: true)
 
     if !exists[:data].nil?
       course = JSON.parse(response.body, symbolize_names: true)
-      # EVENTUALLY, we will need to update for use of current_user for student.id
-      # student = current_user
-      # response = conn("/api/v1/students/#{student.id}").get
-
-      student_id = 1
-        # From postico (priyas local), student #20 is who we are using "Gala Schamberger"
-        # From postico (Saryn's local), student 4 is who grabbed (Fidel Ryan DDS)
+      student_id = 1 # student = current_user[:uid]
       response = conn("/api/v1/students/#{student_id}").get
       student = JSON.parse(response.body, symbolize_names: true)
 
@@ -56,7 +38,7 @@ class Students::CoursesController < ApplicationController
       response = conn("/api/v1/students/courses").post do |request|
         request.body = student_course_creation_params
       end
-      student_course_data = JSON.parse(response.body, symbolize_names: true)
+      JSON.parse(response.body, symbolize_names: true)
       redirect_to students_courses_path
     else
       # Sad Path
