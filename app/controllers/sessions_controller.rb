46baf7c831a854496f016d4dbd5f10096cfedc49
Cam C <ckc.cameron@gmail.com>
2020-11-03 11:53:28 -0800

    Update SessionsController create action to generate JSON for request body
    * changed naming of access_token to user_data within body hash being sent
    
    Co-authored-by: Arique1104 <60521756+Arique1104@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
   def create
     body = {
-      client_id: ENV['GOOGLE_CLIENT_ID'],
-      client_secret: ENV['GOOGLE_CLIENT_SECRET'],
-      access_token: request.env['omniauth.auth']
-    }
+              client_id: ENV['GOOGLE_CLIENT_ID'],
+              client_secret: ENV['GOOGLE_CLIENT_SECRET'],
+              user_data: request.env['omniauth.auth']
+            }
 
     uid = request.env['omniauth.auth'][:uid]
     session[:uid] = uid
 
     response = Faraday.post("http://localhost:3000/api/v1/#{session[:user_type]}") do |request|
-      request.body = body
+      request.body = JSON.generate(body)
     end
 
     if session[:user_type] == 'students'
