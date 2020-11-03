c864b6165cb9259851451a80a7f3551d719fd978
Cam C <ckc.cameron@gmail.com>
2020-11-02 16:34:19 -0800

    Change student_facade fixture naming to say student
    Co-authored-by: Arique1104 <60521756+Arique1104@users.noreply.github.com>

--- spec/facades/student_facade_spec.rb
+++ spec/facades/student_facade_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe StudentFacade do
   it "returns student user object" do
     uid = 1
-    response = File.read("spec/fixtures/student_facade.json")
+    response = File.read("spec/fixtures/student.json")
     stub_request(:get, "http://localhost:3000/api/v1/students/find/#{uid}").to_return(status: 200, body: response, headers: {})
     student = StudentFacade.find(uid)
 
