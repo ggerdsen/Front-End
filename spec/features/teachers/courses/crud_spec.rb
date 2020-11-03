702c7a8c565bcd81dc0cb0e043fd2f96dcf552ff
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-03 14:41:19 -0700

    Add update spec for teacher courses
    - First makes faraday call for coures on teacher id 1
    - Patch request with updated params
    - Checks that course name and school name no longer match old records 
    and match updates
    - Returns database back to original state so that test can be run 
    multiple times

--- spec/features/teachers/courses/crud_spec.rb
+++ spec/features/teachers/courses/crud_spec.rb
@@ -43,6 +43,7 @@ RSpec.describe 'Teachers course CRUD' do
       expect(page).to_not have_content(teacher_course_params[:name])
     end
 
+    # CREATE
     within '#add-courses' do
       fill_in :name, with: teacher_course_params[:name]
       fill_in :school_name, with: teacher_course_params[:school_name]
@@ -61,9 +62,11 @@ RSpec.describe 'Teachers course CRUD' do
     end
     courses = JSON.parse(response.body, symbolize_names: true)
     course1_id = courses[:data].last[:id]
+
+    # DESTROY
     within '#my-courses' do
       within "#course-#{course1_id}" do
-        click_on 'Remove Course'
+        click_on 'Remove this Course'
       end
     end
     expect(current_path).to eq(teachers_courses_path)
@@ -71,5 +74,70 @@ RSpec.describe 'Teachers course CRUD' do
     within '#my-courses' do
       expect(page).to_not have_content(teacher_course_params[:name])
     end
+  end
+
+  scenario 'a teacher can update names on courses' do
+    # stub_omniauth_teacher
+    stub_omniauth
+    visit teachers_courses_path
+
+    # GET COURSE INFORMATION
+    teacher_params = ({teacher_id: 1}) # teacher_params = ({teacher_id: current_user[:uid]})
+    response = Faraday.get('http://localhost:3000/api/v1/teachers/courses') do |request|
+      request.body = teacher_params
+    end
+    courses = JSON.parse(response.body, symbolize_names: true)
+    course1_id = courses[:data][0][:id]
+    course1_name = courses[:data][0][:attributes][:name]
+    course1_school_name = courses[:data][0][:attributes][:school_name]
+    course2_name = courses[:data][1][:attributes][:name]
+    course2_school_name = courses[:data][1][:attributes][:school_name]
+
+    within '#my-courses' do
+      expect(page).to have_content(course1_name)
+      expect(page).to have_content(course1_school_name)
+      expect(page).to have_content(course2_name)
+      expect(page).to have_content(course2_school_name)
+    end
+
+    within '#my-courses' do
+      within "#course-#{course1_id}" do
+        click_on 'Update this Course'
+      end
+    end
+    expect(current_path).to eq("/teachers/courses/#{course1_id}/edit")
+
+    update_params = {
+      name: "This Course has been updated",
+      school_name: "This School Name has been updated"
+    }
+    fill_in :name, with: update_params[:name]
+    fill_in :school_name, with: update_params[:school_name]
+    click_on 'Update Course'
+    expect(current_path).to eq(teachers_courses_path)
+
+    within '#my-courses' do
+      expect(page).to_not have_content(course1_name)
+      expect(page).to_not have_content(course1_school_name)
+      expect(page).to have_content(course2_name)
+      expect(page).to have_content(course2_school_name)
+      expect(page).to have_content(update_params[:name])
+      expect(page).to have_content(update_params[:school_name])
+    end
+
+    # RETURN THE LIVE DATABASE BACK TO ORIGINAL
+    within '#my-courses' do
+      within "#course-#{course1_id}" do
+        click_on 'Update this Course'
+      end
+    end
+
+    update_params = {
+      name: course1_name,
+      school_name: course1_school_name
+    }
+    fill_in :name, with: update_params[:name]
+    fill_in :school_name, with: update_params[:school_name]
+    click_on 'Update Course'
   end
 end
