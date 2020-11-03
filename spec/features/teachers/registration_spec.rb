2b9fb7c28946337e29c549ec3e9d22bdbd1c6576
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-11-03 17:08:48 -0600

    Restarting this test.  Commented out for reference.
    DETAILS: Need to Knows AND Nice to Knows
    
    Co-authored-by: Cam C <57038617+ckccameron@users.noreply.github.com>

--- spec/features/teachers/registration_spec.rb
+++ spec/features/teachers/registration_spec.rb
@@ -1,25 +1,25 @@
-require 'rails_helper'
-
-describe "Teacher Registration" do
-  it 'can request school_name and school_district from teacher' do
-    uid = 102
-    response = File.read("./spec/fixtures/teacher_registration.json")
-    stub_request(:get, "http://localhost:3000/api/v1/teachers/find/#{uid}").to_return(status: 200, body: response, headers: {})
-
-    teacher = TeacherFacade.find(uid)
-
-    visit '/teacher/courses'
-    save_and_open_page
-    within '.teacher_school_info' do
-      fill_in :school_name, with: 'Jose De Diego'
-      fill_in :school_district, with: 'Humboldt Park'
-    end
-    click_button 'Submit'
-
-    expect(current_path).to eq('/teacher/courses')
-
-    expect(page).to have_content("School name: Jose De Diego")
-    expect(page).to have_content("School district: Humboldt Park")
-
-  end
-end
+# require 'rails_helper'
+#
+# describe "Teacher Registration" do
+#   it 'can request school_name and school_district from teacher' do
+#     uid = 102
+#     response = File.read("./spec/fixtures/teacher_registration.json")
+#     stub_request(:get, "http://localhost:3000/api/v1/teachers/find/#{uid}").to_return(status: 200, body: response, headers: {})
+#
+#     teacher = TeacherFacade.find(uid)
+#
+#     visit '/teacher/courses'
+#     save_and_open_page
+#     within '.teacher_school_info' do
+#       fill_in :school_name, with: 'Jose De Diego'
+#       fill_in :school_district, with: 'Humboldt Park'
+#     end
+#     click_button 'Submit'
+#
+#     expect(current_path).to eq('/teacher/courses')
+#
+#     expect(page).to have_content("School name: Jose De Diego")
+#     expect(page).to have_content("School district: Humboldt Park")
+#
+#   end
+# end
