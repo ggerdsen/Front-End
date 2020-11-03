8ccb3141b80ad2b97de431ce9228ac3fb086e71d
Chris Allbritton <cla.inbox@gmail.com>
2020-11-03 14:32:43 -0500

    Remove teacher course controller
    Co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu> 
    Co-authored-by: Priya Power 
    <49959312+priyapower@users.noreply.github.com>

--- app/controllers/teacher/courses_controller.rb
+++
@@ -1,9 +0,0 @@
-class Teacher::CoursesController < ApplicationController
-  def index
-    # response = Faraday.get("BACKEND URI")
-    # courses_data = JSON.parse(response.body, symbolize_names: true)
-    # @courses = courses_data[:data].map do |course_data|
-    #   Course.new(course_data)
-    # end
-  end
-end
