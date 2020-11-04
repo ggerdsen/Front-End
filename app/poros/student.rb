8442a28541a5db81d0c919afeb71e9a286b16d59
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-04 07:28:08 -0700

    Update for merge

--- app/poros/student.rb
+++ app/poros/student.rb
@@ -16,12 +16,11 @@ attr_reader :id,
     @uid = student_info[:data][:attributes][:uid]
     @email = student_info[:data][:attributes][:email]
     @token = student_info[:data][:attributes][:token]
-
     @first_name = student_info[:attributes][:first_name]
     @last_name = student_info[:attributes][:last_name]
     @points = student_info[:attributes][:points]
     @courses = course_data.map { |course| Course.new(course) } if course_data
-  
+    
     @poms = pom_data.map { |pom| Pom.new(pom) } if pom_data
   end
 end
